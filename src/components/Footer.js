316b80b0f518da0765436c011693971033e2837c
Kaiqing <zkqhnfg@gmail.com>
2023-03-22 11:11:41 +0800

    Set up homepage

---
+++ src/components/Footer.js
@@ -0,0 +1,25 @@
+import './Footer.css';
+import logo from '../assets/Logo.svg';
+
+function Footer() {
+  return (
+    <footer>
+      <img src={logo} alt="logo" />
+      <div>
+        <li>
+          <ul>
+            <a href="https://map.google.com">Address: Chicago</a>
+          </ul>
+          <ul>
+            <a href="tel:555-555-5555">Phone: 555-555-5555</a>
+          </ul>
+          <ul>
+            <a href='mailto:'>Email: example@email</a>
+          </ul>
+        </li>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../assets/Logo.svg';
+import './Header.css';
+
+function Header() {
+  return (
+    <header>
+      <img src={logo} alt="logo" />
+    </header>
+  )
+}
+
+export default Header;
